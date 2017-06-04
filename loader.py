ec8fe5572ca4e09f4f883209d01b5a49c4e3b2dc
Gabriel Simoes <gabriel.simoes@gmail.com>
2017-06-03 22:45:55 -0300

    Data loader fixed: letter image.

--- loader.py
+++ loader.py
@@ -25,8 +25,7 @@ class DFDLoader:
         while True:
             im_data = cv2.imread(self.images_paths[instances_index])            
 
-            im_key  = self.images_paths[instances_index].split("/")[-1][:-4]
-            im_data = cv2.resize(im_data, (net_input_dim[0], net_input_dim[1])).astype(np.float32)
+            im_data = self.letter_image(im_data, net_input_dim).astype(np.float32)
             im_data /= 255.0
             im_data = im_data[...,::-1]
 
@@ -73,14 +72,31 @@ class DFDLoader:
                 yield X, files_names
                 files_names = []
 
+    def letter_image(self, im, net_input_dim=(224,224,3)):
+        w = net_input_dim[1]
+        h = net_input_dim[0]
+        im_out = np.ones(net_input_dim) * 127
+
+        new_w = im.shape[1];
+        new_h = im.shape[0];
+        if w/float(im.shape[1]) < h/float(im.shape[0]):
+            new_w = w;
+            new_h = (im.shape[0] * w)/im.shape[1];
+        else:
+            new_h = h;
+            new_w = (im.shape[1] * h)/im.shape[0];
+
+        im_out[(h - new_h) / 2:new_h + (h - new_h) / 2,(w - new_w) / 2:new_w + (w - new_w) / 2,:] = cv2.resize(im, (new_w, new_h))
+        return im_out
+
 if __name__ == "__main__":
     list_path     = "/home/gabriel/datasets/X_Dataset_segmentation_3K_VOC/VOC2007/train.txt"
     # features_path = "/home/gabriel/python_code/yolo_depthwise/features.dat"
     # dfd = DFDLoader(list_path, features_path)
     dfd = DFDLoader(list_path)
-    gen = dfd.data_generator(10, shuffle=True)
-    gen = dfd.test_data_generator(10)
+    gen = dfd.data_generator(10, shuffle=True, net_input_dim=(416, 416, 3))
+    # gen = dfd.test_data_generator(10)
 
     for i in range(1000):
         data = gen.next()
-        print data[1]
+        # print data[1]
